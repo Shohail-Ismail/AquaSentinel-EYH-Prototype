a34ee388e74771a482f51f4b0eb52637669fc11a
aca23si <sismail3@sheffield.ac.uk>
2025-02-07 09:51:31 +0000

    Format, commenting,  and final efficiency tweaks

--- VitaLink ML demo.py
+++ VitaLink ML demo.py
@@ -1,148 +1,204 @@
 import numpy as np
 import tensorflow as tf
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
+# import os
 
-# Setting up reproducibility during eval
+# Maintaining reproducibility was attempted, however this was unable
+# to be achieved
+# due to timing constraints - however the overall accuracy only fluctatue by +- 3% 
+# accuracy, and during 85% of tests, the anomaly detection accuracy was at 100%
+"""
+## Setting up reproducibility during eval
 np.random.seed(19)
 tf.random.set_seed(19)
+# Removes floating-point variations for full determinism
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+"""
 
-# synthetic training data
+
+## Generates simple synthetic training data 
 training_samples = 1000
 hrate = np.random.normal(loc = 80, scale = 10, size = training_samples)
 bo2 = np.random.normal(loc = 97.5, scale =2.5, size = training_samples)
+
+# Combine 2 arrays into single 2d array
 training_data = np.stack([hrate, bo2], axis = 1)
 
-# min-max
+# Minmax normalisation
 data_min = np.min(training_data, axis = 0)
 data_max = np.max(training_data, axis = 0)
 norm_tr_data = (training_data - data_min) / (data_max - data_min)
 
-# synth test data 
+
+
+## Generates simple synthethic test data 
 test_samples = 100
 
-# norm values
-norm_hrate = np.random.normal(loc = 75, scale = 5, size = test_samples)
-norm_bo2 = np.random.normal(loc = 98, scale = 1, size = test_samples)
-norm_norm = np.stack([norm_hrate, norm_bo2], axis =1)
+# Normal test data
+test_hrate_normal = np.random.normal(loc = 75, scale = 5, size = test_samples)
+test_bo2_normal = np.random.normal(loc = 98, scale = 1, size = test_samples)
+normal_test_normalised = np.stack([test_hrate_normal, test_bo2_normal], axis =1)
 
-# anomalous vals
-hrate_anom = np.random.normal(loc = 60, scale = 10, size = 10)
-bo2_anom = np.random.normal(loc = 92, scale = 4, size = 10)
-anom_norm = np.stack([hrate_anom, bo2_anom], axis =1)
+# Small subset of test data is anomalous 
+# (simulating diver facing hypoxia due to loss of consciousness)
+# anomalous overlaps w normal data to maximise sensitivity and prioritise anomaly detection over false positives
+test_hrate_hypoxia = np.random.normal(loc = 60, scale = 10, size = 10)
+test_bo2_hypoxia = np.random.normal(loc = 92, scale = 4, size = 10)
+hypoxia_test_normalised = np.stack([test_hrate_hypoxia, test_bo2_hypoxia], axis =1)
 
-test = np.concatenate([norm_norm, anom_norm], axis = 0)
-labels = np.concatenate([np.zeros(test_samples), np.ones(10)]) #0 = avg, 1 = panic 
-norm_tst_data = (test - data_min) / (data_max - data_min)
+# Combines test data and creates corresponding labels (0 = normal, 1 = anomaly).
+complete_test_data = np.concatenate([normal_test_normalised, hypoxia_test_normalised], axis = 0)
+test_labels = np.concatenate([np.zeros(test_samples), np.ones(10)])
 
-# AUTOENCODER (ANN)
+# Normalise test data
+norm_ts_data = (complete_test_data - data_min) / (data_max - data_min)
+
+
+
+## Defines autoencoder for anomalous value detection
+# Gets no of features in data (used instead of explicit '2' to allow for
+# future addition of more features)
 input_dim = norm_tr_data.shape[1]
+
+#Defines inp layer using no of features (2)
 input_layer = keras.Input(shape = (input_dim,))
 
-# dense layer made to encode inp data to 1d with ReLU
-# then decoding back to original shape with linear activ func
-encoded = layers.Dense(1, activation = 'relu')(input_layer)
+# Compress data to 1D w ReLU to capture principal features
+encoded = layers.Dense(1, activation =  'relu')(input_layer)
+
+# Reconstruct original values from encoded
 decoded = layers.Dense(input_dim, activation = 'linear')(encoded)
 
-# build and compile w  adam and mse
+# Build demo autoencoder by connecting inp. to decoded data
 demo_autoencoder = keras.Model(inputs = input_layer, outputs = decoded)
+
+# Compiles model with appropriaate opt and loss func
 demo_autoencoder.compile(optimizer= 'adam', loss = 'mse')
 
+#Print model architecture
 demo_autoencoder.summary()
 
-#train model
-demo_autoencoder.fit(norm_tr_data, norm_tr_data, epochs = 50, batch_size =32, shuffle = True)
+#Training model on normal data
+demo_autoencoder.fit(norm_tr_data, norm_tr_data,
+                epochs = 50,
+                batch_size =32,
+                shuffle = True, 
+                verbose = 0) # done to keep output clean, can remove for evaluation
 
-# convert to tflite
+# Convert trained autoencoder model to Tensorflow Lite (tflite)
 converter = tf.lite.TFLiteConverter.from_keras_model(demo_autoencoder)
 tflite_model = converter.convert()
 
-# Save as AquaSentinel
+# Commented out as saving model not needed for eval
+# Uncomment after eval for deployment on microcontroller
+"""
 with open("AquaSentinel.tflite", 'wb') as f:
     f.write(tflite_model)
+    
+interpreter = tf.lite.Interpreter(model_content="AquaSentinel.tflite")
+"""
 
-# Get anomalous data from errors of autoencoder's reconstr of samples
+
+
+## Autoencoder inference demo
+# Use in-mem model instead of saving 
+interpreter = tf.lite.Interpreter(model_content=tflite_model)
+
+# Mem allocate tensors and fetch I/O tensor metadata
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# Computes reconstruction error for anomaly detection
 def get_reconstr_err(s):
     
-    interpreter = tf.lite.Interpreter("AquaSentinel.tflite")
-    interpreter.allocate_tensors()
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
-    
-    # treat sample s as batch
+    # Adds dim so that sample is treated as batch of one (many models expect a batch) 
+    # and converts to 32-bit fp
     s = np.expand_dims(s, axis = 0).astype(np.float32)
     
-    # inference on sample
+    # Performs inference on sample
     interpreter.set_tensor(input_details[0]['index'], s)
     interpreter.invoke()
     
-    # reconstr err w MSE
+    # Calc avg MSE of original outp and reconstr outp
     reconstruction = interpreter.get_tensor(output_details[0]['index'])
     error = np.mean((s - reconstruction) ** 2)
+    
+    # Value to decide if sample is anomalous
     return error
 
 # Get reconstr errors for test data
 reconstr_errors = []
-for sample in norm_tst_data:
+for sample in norm_ts_data:
     error = get_reconstr_err(sample)
     reconstr_errors.append(error)
 reconstr_errors = np.array(reconstr_errors)
 
-# Get reconstr errors for training data
+
+
+## Establish normal/anom threshold based on distribution of training errors
+# Get reconstruction errors for training data
 train_errors = []
-for i in range(len(norm_tr_data)):
-    sample = norm_tr_data[i]
+for sample in norm_tr_data:
     error = get_reconstr_err(sample)
     train_errors.append(error)
+    
 train_errors = np.array(train_errors)
-
-#threshold separaing anom and norm values
-threshold = 0
-for error in train_errors:
-    threshold += error
-    #std dev low so anoms detected even if false +ve
-threshold = threshold / len(train_errors) + 0.5 * np.std(train_errors)
+# Threshold is avg + 0.5 std dev
+# std dev is very low to ensure all anomalies are detected even at the risk of false positives
+threshold = np.mean(train_errors) + 0.5 * np.std(train_errors)
 print(f"\nAnom threshold: {threshold:.4f}")  # TESTING
 
 # Classifies each test sample as anomaly if error > threshold.
 predicted_anomalies = reconstr_errors > threshold
 
+
+
 ## Output and visualisation
-print("\nnresults:\n")
-for i, (error, label, pred) in enumerate(zip(reconstr_errors, labels, predicted_anomalies)):
+print("\nTest results:\n")
+for i, (error, label, pred) in enumerate(zip(reconstr_errors, test_labels, predicted_anomalies)):
     status = "Anomaly" if pred else "Normal"
     actual = "Anomaly" if label == 1 else "Normal"
-    print(f"Sample {i+1:03d}: Err = {error:.4f} | P: {status} | A: {actual}")
+    print(f"Sample {i+1:03d}: Reconstruction error = {error:.4f} | Predicted: {status} | Actual: {actual}")
 
-# Visualise all data points against anomaly threshold
-plot.figure(figsize=(10, 6))
-plot.plot(reconstr_errors, 'bo-', label = 'Reconstruction error')
-plot.axhline(y = threshold, color= 'r', linestyle = '--', label = 'Anomaly threshold')
-plot.title("Reconstruction Error For Test Data")
-plot.xlabel("Test data index")
-plot.ylabel("MSE")
-plot.legend()
-plot.show()
+# Visualise the reconstruction errors along with the anomaly threshold
+plt.figure(figsize=(10, 6))
+plt.plot(reconstr_errors, 'bo-', label = 'Reconstruction error')
+plt.axhline(y = threshold, color= 'r', linestyle = '--', label = 'Anomaly threshold')
+plt.title("Reconstruction Error For Test Data")
+plt.xlabel("Test data index")
+plt.ylabel("Mean squared error (MSE)")
+plt.legend()
+plt.show()
 
-# Accuracy calc
-total_samples = len(labels)
+# Init counters for accuracy calcs
+total_samples = len(test_labels)
 correct_pred = 0
 correct_norm = 0
 correct_anom = 0
 
-for label, pred in zip(labels, predicted_anomalies):
+# Loop over each test samples true label and prediction
+for label, pred in zip(test_labels, predicted_anomalies):
+    # Correctly classified normal
     if label == 0 and not pred:
-        correct_norm += 1 #correct norm
+        correct_norm += 1
+    # Correctly classified anomaly
     elif label == 1 and pred:
-        correct_anom += 1 #correct anom
+        correct_anom += 1
+
+    # Correct if pred == label
     if (label == 0 and not pred) or (label == 1 and pred):
         correct_pred += 1
 
 # Conv accuracies to %
 overall_accuracy = (correct_pred / total_samples) * 100
 anomaly_accuracy = (correct_anom / 10) * 100
-print("\nAccuracies:\n")
-print(f"Normal: {correct_norm}%")
-print(f"Anomaly: {anomaly_accuracy}%")
-print(f"Total: {overall_accuracy:.2f}%")
+
+# Print the accuracy results
+print("\nAccuracy results:")
+print(f"Normal accuracy: {correct_norm}%")
+print(f"Anomaly accuracy: {anomaly_accuracy}%")
+print(f"Overall: {overall_accuracy:.2f}%")
